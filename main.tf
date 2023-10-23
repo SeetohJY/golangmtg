058312d9a6cb4436ea9d6721421399afb0f774db
jianyang.s <jianyang.s@affinidi.com>
2023-10-23 17:10:31 +0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_storage_bucket" "mtgjsondata" {
+  name          = "auto-expiring-mtgjsondata-bucket"
+  location      = "US"
+  storage_class = "STANDARD"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 2
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
