1dd86f31e2c6271ced04d4ece09a3fbe4d9b4012
jianyang.s <jianyang.s@affinidi.com>
2023-10-24 21:13:17 +0800

    fix: add gitignore

--- main.tf
+++ main.tf
@@ -15,32 +15,6 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_storage_bucket" "mtgjsondata" {
-  name          = "auto-expiring-mtgjsondata-bucket"
-  location      = "US"
-  storage_class = "STANDARD"
-  force_destroy = true
-
-  lifecycle_rule {
-    condition {
-      age = 2
-    }
-    action {
-      type = "Delete"
-    }
-  }
-
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "AbortIncompleteMultipartUpload"
-    }
-  }
-}
-
-
 resource "google_project_service" "firestore" {
   project = var.project
   service = "firestore.googleapis.com"
@@ -48,7 +22,7 @@ resource "google_project_service" "firestore" {
 
 resource "google_firestore_database" "database" {
   project     = var.project
-  name        = "(default)"
+  name        = "mtgjson-database"
   location_id = "nam5"
   type        = "FIRESTORE_NATIVE"
 
