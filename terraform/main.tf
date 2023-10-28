67d5e4aec94372a293a32a32a49958f1433e1ada
jianyang.s <jianyang.s@affinidi.com>
2023-10-28 18:10:11 +0800

    fix: terraform changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "google" {
   credentials = file(var.credentials_file)
 
-  project = var.project
+  project = var.env.project
   region  = var.region
   zone    = var.zone
 }
